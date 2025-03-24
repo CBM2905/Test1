8c784962317b773c030829d3cabf3f1dc7cc1aee
CBM <cbmillos29@gmail.com>
2025-03-24 17:07:02 -0500

    Commit

--- src/app/terremoto/terremoto.page.ts
+++ src/app/terremoto/terremoto.page.ts
@@ -20,15 +20,24 @@ export class TerremotoPage {
 
 
   private initMap(lat: any, lon: any, mag: any, details: any): void {
-   
+    var greenIcon = L.icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+      shadowUrl: '',
+  
+      iconSize:     [35, 48], // size of the icon
+      shadowSize:   [50, 64], // size of the shadow
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [4, 62],  // the same for the shadow
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
     const map = L.map('map').setView( [lon, lat], 13);
     map.on("load", function(){
       setTimeout(() => {
         map.invalidateSize();
       }, 10)
     })
-    var marker = L.marker([lon, lat]).addTo(map) // "Kyiv" is the accessible name of this marker
-      .bindPopup("Aqui fue el lugar del terremoto con una magnitud de:" + mag + "mas info + " + details);
+    var marker = L.marker([lon, lat], {icon: greenIcon}).addTo(map) // "Kyiv" is the accessible name of this marker
+      .bindPopup("Aqui fue el lugar del terremoto con una magnitud de:" + mag + "mas info" + details);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
